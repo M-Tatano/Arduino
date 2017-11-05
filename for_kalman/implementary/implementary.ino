54e34b250b45016318c630c3803ebd6926d1d951
M-Tatano <mamorutatano@outlook.jp>
2017-11-05 23:14:21 +0900

    implementary filter

--- for_kalman/implementary/implementary.ino
+++ for_kalman/implementary/implementary.ino
@@ -16,23 +16,42 @@ void setup() {
     CurieIMU.setGyroRange(250);
     
     // Set the accelerometerRange to 2G
-    CurieIMU.setAccelerometerRange(250);
+    CurieIMU.setAccelerometerRange(2);
     }
 
 void loop() {
   float gx, gy, gz;  //scaled Gyro values 
   float ax, ay, az;  //scaled accelerometer values
   const dt=0.005;
+  float angleX, angleY, angleZ;
+  float angleXaccel, angleYaccel, angleZaccel;
 
   // read gyro measurements from device, scaled to the configured range
   CurieIMU.readGyroScaled(gx, gy, gz);
   
+  // calculation of angle by accelometer
+  
   // read accelerometer measurements from device, scaled to the configured range
   CurieIMU.readAccelerometerScaled(ax, ay, az);
 
+  // calculation of angle by accelometer
+  angleXaccel = atan((double)ax/sqrt(pow((double)ay,2)+pow((double)az,2)));
+  angleYaccel = atan((double)ay/sqrt(pow((double)ax,2)+pow((double)az,2)));
+  angleZaccel = atan((double)az/sqrt(pow((double)ax,2)+pow((double)ay,2)));
+
+
+  // simpified implementary filter 
+  angleX = 0.95*(angleX+gx*dt)+0.05*angleXaccel;
+  angleY = 0.95*(angleY+gy*dt)+0.05*angleYaccel;
+  angleZ = 0.95*(angleZ+gz*dt)+0.05*angleZaccel;
+
 //dispaly sensored values
 
-  Serial.print("g:\t");
+  char outs[32];
+  sprintf(outs, "angleX = %f, angleY = %f, angZ = %f\n", angleX, angleY, angleZ);  Serial.print(outs);
+  delay(100);
+
+/*Serial.print("g:\t");
   Serial.print(gx);
   Serial.print("g:\t");
   Serial.print(gy);
@@ -47,5 +66,5 @@ void loop() {
   Serial.print("a:\t");
   Serial.print(az);
   Serial.println();
-
+*/
 }
